5b2f9e2f09a3f6853c770e0979b98b84a377b59e
sarvaiyarenish <sarvaiyarenish@gmail.com>
2026-01-06 22:30:08 +0530

    Refactor nums_and_math.js and add dates.js with date manipulation examples

--- Basics/nums_and_math.js
+++ Basics/nums_and_math.js
@@ -1,17 +1,37 @@
 const score = 200
 
-console.log(score);
+// console.log(score);
 
 const balance = new Number(200)
-console.log(balance);
+// console.log(balance);
 
-console.log(typeof balance.toString());
-console.log(balance.toString().length);
-console.log(balance.toFixed(2));
+// console.log(typeof balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
 
 const otherNumber = 29.86
 
-console.log(otherNumber.toPrecision(3));
+// console.log(otherNumber.toPrecision(3));
 
 const one = 200000
-console.log(one.toLocaleString('en-In'));
+// console.log(one.toLocaleString('en-In'));
+
+// ++++++++++++++++ Maths ++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(85.9));
+// console.log(Math.ceil(84.5));
+// console.log(Math.floor(84.3));
+// console.log(Math.sqrt(81));
+// console.log(Math.min(5,9,6,7,8,2));
+// console.log(Math.max(9,6,3,2,5,8,7,4));
+
+console.log(Math.random());
+console.log(Math.random()*10 + 1);
+console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
